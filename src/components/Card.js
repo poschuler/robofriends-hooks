7d731e0471d53b345feff4b0e5ba44b0cf332110
poschuler <poschuler@gmail.com>
2023-02-22 18:38:50 -0500

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+const Card = (props) => {
+  const { name, email, id } = props;
+  return (
+    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img alt="robots" src={`https://robohash.org/${id}?200x200`} />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
+
+Card.propTypes = {};
+
+export default Card;
